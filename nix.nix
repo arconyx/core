8c9717c761cb498c76e692d31eefa5874fef7102
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-07-15 12:26:01 +1200

    enable fallback on most systems

--- nix.nix
+++ nix.nix
@@ -1,24 +1,24 @@
-{ ... }:
+{ config, ... }:
 {
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   nix.settings = {
-    # Enable the Flakes feature and the accompanying new nix command-line tool
+    auto-optimise-store = true;
     experimental-features = [
       "nix-command"
       "flakes"
     ];
-
+    # The most common scenario in which this is useful is when we have registered substitutes
+    # in order to perform binary distribution from, say, a network repository.
+    # If the repository is down, the realisation of the derivation will fail.
+    # When this option is specified, Lix will build the derivation instead.
+    # Thus, installation from binaries falls back on installation from source.
+    # This option is not the default since it is generally not desirable for a transient failure
+    # in obtaining the substitutes to lead to a full build from source (with the related consumption of resources).
+    # - [Lix Docs](https://docs.lix.systems/manual/lix/stable/command-ref/opt-common.html)
+    fallback = !config.arcworks.server.pi;
     trusted-users = [ "@wheel" ];
-
-    # Optimize storage
-    # You can also manually optimize the store via:
-    #    nix-store --optimise
-    # Refer to the following link for more details:
-    # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
-    auto-optimise-store = true;
-
   };
 
   # Perform garbage collection to maintain low disk usage
