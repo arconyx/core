aa0eb81e6891e86adcbd142ea36e6450f36cfb9f
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-08-07 12:48:41 +1200

    Enable notifications from kitty

--- home/desktop/kitty.nix
+++ home/desktop/kitty.nix
@@ -8,7 +8,8 @@
       enable = true;
       themeFile = "Monokai_Soda";
       settings = {
-        "scrollback_pager_history_size" = 20; # MB. Approx 200,000 lines. See https://sw.kovidgoyal.net/kitty/conf/#opt-kitty.scrollback_pager_history_size
+        scrollback_pager_history_size = 20; # MB. Approx 200,000 lines. See https://sw.kovidgoyal.net/kitty/conf/#opt-kitty.scrollback_pager_history_size
+        notify_on_cmd_finish = "invisible";
       };
     };
   };
