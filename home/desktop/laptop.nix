4edc8d8d7da7547ed94b6948033ca0ea19bb3d54
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-10-23 10:20:29 +1300

    Update poweralertd options

--- home/desktop/laptop.nix
+++ home/desktop/laptop.nix
@@ -6,8 +6,11 @@
       enable = true;
       # -s ignore events at startup
       # -S only use events coming from power supplies
+      # `-i line power` ignore events from charging cable
       extraArgs = [
         "-s"
+        "-i"
+        "line power"
         # "-S"
       ];
     };
