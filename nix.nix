1de29f598382e29414bd71c3eacc0dce712cca86
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2026-04-25 15:41:00 +1200

    Use xdg directories for nix
    Closes #2

--- nix.nix
+++ nix.nix
@@ -44,6 +44,7 @@
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    use-xdg-base-directories = true;
   };
 
   # Perform garbage collection to maintain low disk usage
